33180f902abcc422e5c7c3ce1986cfe7ade34110
fibophi <phibologic@gmail.com>
2025-08-06 16:03:22 -0400

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -45,6 +45,9 @@ export default function ContactPage() {
               >
                 HOW IT WORKS
               </Link>
+              <Link href="/blog" className="text-gray-300 hover:text-yellow-400 font-semibold transition-colors">
+                BLOG
+              </Link>
               <Link href="/contact" className="text-white font-semibold border-b-2 border-yellow-400 pb-1">
                 CONTACT
               </Link>
